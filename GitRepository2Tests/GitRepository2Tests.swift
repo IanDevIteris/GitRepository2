edcad07463e9131acd9666acb7fd49d387b0f739
Ian Vilhena <ian.vilhena@iteris.com.br>
2018-10-16 14:17:18 -0300

    Initial Commit

---
+++ GitRepository2Tests/GitRepository2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  GitRepository2Tests.swift
+//  GitRepository2Tests
+//
+//  Created by Ian Vilhena on 16/10/18.
+//  Copyright © 2018 Ian Vilhena. All rights reserved.
+//
+
+import XCTest
+@testable import GitRepository2
+
+class GitRepository2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
